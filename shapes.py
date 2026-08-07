ac365f9eb665f32a02e6d7a1954a01e13ea8d315
Kyle Kestell <kkestell@livefront.com>
2026-08-06 19:34:24 -0500

    Add area helpers

---
+++ shapes.py
@@ -0,0 +1,13 @@
+"""A few shapes, for Coral to have something to look at."""
+
+
+def rectangle_area(width, height):
+    return width * height
+
+
+def triangle_area(base, height):
+    return base * height / 2
+
+
+def circle_area(radius):
+    return 3.14159 * radius * radius
